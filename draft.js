a4ebbd3ee6b0c54f3f64b878a36343dd3a28d573
Alberto Bastos <alberto.bastos@devo.com>
2019-11-30 10:36:37 +0100

    authors, contents template and draft script

---
+++ draft.js
@@ -0,0 +1,12 @@
+// Sorteo de la rotación para proponer temas
+
+const authors = require('./authors.json');
+
+const names = Object.values(authors);
+
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+for(let i=0;i<names.length*100;i++) shuffle(names);
+
+names.forEach((name, index) => {
+  console.log(`#${index+1}: ${name}`);
+});
