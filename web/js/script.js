0b5a08bc2f7f0c7b3067bb892f0718e843921c95
Alberto Bastos <alberto.bastos@devo.com>
2019-12-03 09:56:44 +0100

    get contents json

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,37 @@
 var app = new Vue({ 
   el: '#app',
   data: {
-      message: 'Hello project!'
+      authors: false,
+      weeks: false
   },
-  template: '<div>{{message}}</div>'
+  template: `
+<div>
+  <h1>Authors</h1>
+  {{ authors ? Object.keys(authors).length + ' authors' : 'Loading...' }}
+  <h1>Weeks</h1>
+  {{ weeks ? weeks.length + ' weeks' : 'Loading...' }}  
+</div>`
 });
+
+getData(function(err, data) {
+  app.authors = data.authors;
+  app.weeks = data.weeks;
+});
+
+function getData(callback) {
+  let request = new XMLHttpRequest();
+  request.open('GET', './data.json', true);
+  request.onerror = function() {
+    let err = new Error('Error during data retrieval.');
+    callback(err);
+  }
+  request.onload = function() {
+    if(request.status >= 200 && request.status < 400) {
+      callback(null, JSON.parse(request.responseText));
+    } else {
+      let err = new Error('Unexpected HTTP status retrieving data: ' + request.status);
+      callback(err);
+    }
+  }
+  request.send();
+}
