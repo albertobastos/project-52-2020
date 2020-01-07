64a8f2f484009ac9f8f74b6bdb465e3bdb70b09f
aNieto2k <andres.nieto@myhappyforce.com>
2020-01-07 13:08:15 +0100

    Add firebase storage

---
+++ firebase.js
@@ -0,0 +1,39 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAk2hB67Df0Xcq2HvXJGcmht0QZTtLp5OQ",
+    authDomain: "weeks-f9bd8.firebaseapp.com",
+    databaseURL: "https://weeks-f9bd8.firebaseio.com",
+    projectId: "weeks-f9bd8",
+    storageBucket: "weeks-f9bd8.appspot.com",
+    messagingSenderId: "870989957053",
+    appId: "1:870989957053:web:2c37444d4b6cb721e074d4",
+    measurementId: "G-849LEETYHW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var storage = firebase.storage();
+
+
+function getFirebaseData(callback) {
+    database.ref('/').once('value').then(function(snapshot) {
+        const data = (snapshot.val() && snapshot.val()) || 'Anonymous';
+        callback(null, data)
+    });
+};
+
+function saveOnFirebase(weeks) {
+    database.ref('/week')
+    firebase.database().ref('weeks').set(weeks);
+}
+
+function storageBackupOnFirebase(json) {
+    const filename = `${moment().format('DDMMYYYY_HH:mm:ss')}.json`;
+
+    var storageRef = storage.ref().child(filename);
+    storageRef.putString(JSON.stringify(json)).then(function(snapshot) {
+        location.href="/";
+        alert("Yeah!! Ya queda una foto menos :D");
+     });
+}
